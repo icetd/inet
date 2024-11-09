a7654730769aceb35028d996e5e12245512997dd
icetd <tiandengzbc@gmail.com>
2024-11-09 12:44:17 +0800

    update

--- src/net/Thread.cc
+++ src/net/Thread.cc
@@ -4,33 +4,33 @@
 using namespace inet;
 
 Thread::Thread(ThreadFunc func) : m_started(false),
-	m_joined(false),
-	m_func(std::move(func)),
-	m_latch(1)
+    m_joined(false),
+    m_func(std::move(func)),
+    m_latch(1)
 {
 }
 
 Thread::~Thread()
 {
-	if (m_started && !m_joined)
-		m_thread.detach();
+    if (m_started && !m_joined)
+        m_thread.detach();
 }
 
 void Thread::start()
 {
-	m_started = true;
+    m_started = true;
 
-	m_thread = std::move(std::thread([this]() {
-		m_tid = CurrentThread::tid();
-		m_latch.countDown();
+    m_thread = std::move(std::thread([this]() {
+        m_tid = CurrentThread::tid();
+        m_latch.countDown();
 		m_func(); }));
-	m_latch.wait();
+    m_latch.wait();
 }
 
 void Thread::join()
 {
-	if (m_thread.joinable()) {
-		m_thread.join();
-		m_joined = true;
-	}
+    if (m_thread.joinable()) {
+        m_thread.join();
+        m_joined = true;
+    }
 }
